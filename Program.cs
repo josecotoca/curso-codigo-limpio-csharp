7586a103169d382cb0beb8381bf055a789f105a2
Jose Carlos HR <josecarlos@srlpunto.com>
2023-04-01 05:25:05 -0400

    Rename variables and methods

--- Program.cs
+++ Program.cs
@@ -5,28 +5,28 @@ namespace ToDo
 {
     internal class Program
     {
-        public static List<string> TL { get; set; }
+        public static List<string> TaskList { get; set; }
 
         static void Main(string[] args)
         {
-            TL = new List<string>();
-            int variable = 0;
+            TaskList = new List<string>();
+            int optionMenu = 0;
             do
             {
-                variable = ShowMainMenu();
-                if (variable == 1)
+                optionMenu = ShowMainMenu();
+                if (optionMenu == 1)
                 {
-                    ShowMenuAdd();
+                    ShowTaskAdd();
                 }
-                else if (variable == 2)
+                else if (optionMenu == 2)
                 {
-                    ShowMenuDos();
+                    ShowTaskRemove();
                 }
-                else if (variable == 3)
+                else if (optionMenu == 3)
                 {
-                    ShowMenuTres();
+                    ShowTaskList();
                 }
-            } while (variable != 4);
+            } while (optionMenu != 4);
         }
         /// <summary>
         /// Show the main menu 
@@ -46,15 +46,15 @@ namespace ToDo
             return Convert.ToInt32(line);
         }
 
-        public static void ShowMenuDos()
+        public static void ShowTaskRemove()
         {
             try
             {
                 Console.WriteLine("Ingrese el número de la tarea a remover: ");
                 // Show current taks
-                for (int i = 0; i < TL.Count; i++)
+                for (int i = 0; i < TaskList.Count; i++)
                 {
-                    Console.WriteLine((i + 1) + ". " + TL[i]);
+                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
                 }
                 Console.WriteLine("----------------------------------------");
 
@@ -63,10 +63,10 @@ namespace ToDo
                 int indexToRemove = Convert.ToInt32(line) - 1;
                 if (indexToRemove > -1)
                 {
-                    if (TL.Count > 0)
+                    if (TaskList.Count > 0)
                     {
-                        string task = TL[indexToRemove];
-                        TL.RemoveAt(indexToRemove);
+                        string task = TaskList[indexToRemove];
+                        TaskList.RemoveAt(indexToRemove);
                         Console.WriteLine("Tarea " + task + " eliminada");
                     }
                 }
@@ -76,13 +76,13 @@ namespace ToDo
             }
         }
 
-        public static void ShowMenuAdd()
+        public static void ShowTaskAdd()
         {
             try
             {
                 Console.WriteLine("Ingrese el nombre de la tarea: ");
                 string task = Console.ReadLine();
-                TL.Add(task);
+                TaskList.Add(task);
                 Console.WriteLine("Tarea registrada");
             }
             catch (Exception)
@@ -90,18 +90,18 @@ namespace ToDo
             }
         }
 
-        public static void ShowMenuTres()
+        public static void ShowTaskList()
         {
-            if (TL == null || TL.Count == 0)
+            if (TaskList == null || TaskList.Count == 0)
             {
                 Console.WriteLine("No hay tareas por realizar");
             } 
             else
             {
                 Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TL.Count; i++)
+                for (int i = 0; i < TaskList.Count; i++)
                 {
-                    Console.WriteLine((i + 1) + ". " + TL[i]);
+                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
                 }
                 Console.WriteLine("----------------------------------------");
             }
